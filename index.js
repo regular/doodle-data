336f3296586c140d17e5a0f0be5d3d4d957edbe3
Jan Bölsche <regular.gonzales@googlemail.com>
2015-09-10 19:02:20 +0200

    Works

---
+++ index.js
@@ -0,0 +1,28 @@
+var hyperquest = require('hyperquest');
+var concat = require('concat-stream');
+var regescape = require('regex-escape');
+
+var prefix = "http://doodle.com/poll/";
+
+module.exports = function(pollId, cb) {
+    var magic = '{"poll":{"prettyUrl":"' + prefix + pollId.substr(0,16) + '"';
+    hyperquest(prefix + pollId + "/admin").pipe(concat(function(buffer) {
+        var page = buffer.toString('utf-8');
+        var re = "(" + regescape(magic)+".*)\\s*\\)\\s*;$";
+        var match = page.match(new RegExp(re,'m'));
+        if (!match) {
+            return cb(new Error('magic string not found in response from doodle.com'));
+        }
+        var result;
+        try {
+            result = JSON.parse(match[1]);
+        } catch(err) {
+            return cb(new Error('JSON parse error in response from doodle.com' + err.message));
+        }
+        cb(null, result);
+    }));
+};
+
+module.exports("s67xgqa7xy3hn7i88rsy2rtz", function(err, data) {
+    console.log(data);
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,16 @@
 # doodle-data
 [![NPM](https://nodei.co/npm/doodle-data.png)](https://nodei.co/npm/doodle-data/)
+
+## Usage
+
+``` js
+var doodle = require('doodle-data');
+doodle("<23-character poll id>", function(data) {
+    console.log(data); // ==> big undocumented JSON thing
+});
+```
+
+You find the 23-character `poll id` by following the "Administer Poll" link in the confirmation mail. It's in the URL.
+
+## License
+MIT
